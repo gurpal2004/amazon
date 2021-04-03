ee11b2620da935c1dee3902486519664ca3f870c
teck-deck-beck <paljai2468@gmail.com>
2021-04-03 07:56:16 +0700

    'start'

---
+++ src/data.js
@@ -0,0 +1,82 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Nike slim shirt',
+            category:'shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high qualtiy product'
+        },
+        {
+            _id:'2',
+            name:'Nike slim shirt',
+            category:'shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high qualtiy product'
+        }, 
+             {
+            _id:'3',
+            name:'Nike slim shirt',
+            category:'shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high qualtiy product'
+        },
+        {
+            _id:'4',
+            name:'Nike slim shirt',
+            category:'shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high qualtiy product'
+        },
+        {
+            _id:'5',
+            name:'Nike slim shirt',
+            category:'shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high qualtiy product'
+        },
+        {
+            _id:'6',
+            name:'Nike slim shirt',
+            category:'pants',
+            image:'/images/p2.jpg',
+            price:120,
+            brand:'nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high qualtiy product'
+        },
+        {
+            _id:'7',
+            name:'Nike slim shirt',
+            category:'pants',
+            image:'/images/p2.jpg',
+            price:120,
+            brand:'nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high qualtiy product'
+        }
+    ]
+}
+export default data
